b008e6f0d05ef98fbf930f83ed5e694b9ed4dbdd
eeluque <eduardoluque08@gmail.com>
2024-11-11 22:21:09 -0600

    creating my own textfield widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,14 +21,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Column(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
           children: [
             Center(
               child: Text("Cerveceria Almendra",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-            )
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+            ),
+            MyTextField(
+              myLabel: Text("Usuario"),
+              prefixIcon: Icon(Icons.account_circle),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({
+    super.key,
+    required this.prefixIcon,
+    required this.myLabel,
+  });
+  final Text myLabel;
+  final Icon prefixIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: prefixIcon,
+          label: myLabel,
+        ),
+      ),
+    );
   }
 }
